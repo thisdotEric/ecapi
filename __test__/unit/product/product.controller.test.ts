a7cf9e4799840c09ae3fddea37ac3499c3070905
thisdotEric <siguenzajohneric@gmail.com>
2022-08-08 01:20:41 +0800

    Implemented unit tests for get single and get all products in product controller and service class

--- __test__/unit/product/product.controller.test.ts
+++ __test__/unit/product/product.controller.test.ts
@@ -4,6 +4,12 @@ import { IProductService } from '../../../src/modules/product/product.interface'
 import { ICreatedProduct } from '../../../src/modules/product/product.model';
 import { createProductInput } from '../../helpers';
 
+type User = {
+  user: {
+    user_id: string;
+  };
+};
+
 describe('Product controller test', () => {
   const res: Partial<Response> = {
     json: jest.fn().mockImplementation((data) => data),
@@ -21,13 +27,18 @@ describe('Product controller test', () => {
           ...productInput,
         };
 
-        const mockedProductService: IProductService = {
+        const mockedProductService: Partial<IProductService> = {
           create: jest.fn().mockReturnValue(newProduct),
         };
+
+        // @ts-ignore
         const productController = new ProductController(mockedProductService);
 
-        const req: Partial<Request> = {
+        const req: Partial<Request & User> = {
           body: productInput,
+          user: {
+            user_id: 'user_id',
+          },
         };
 
         // @ts-ignore
@@ -36,6 +47,79 @@ describe('Product controller test', () => {
         expect(req.body).not.toBe(null);
         expect(res.json).toHaveBeenCalledWith(newProduct);
         expect(res.status).toHaveBeenCalledWith(201);
+      });
+    });
+  });
+
+  describe('get()', () => {
+    describe('user has provided product id', () => {
+      test('should return the product associated with the product_id and status code 200', async () => {
+        const productInput = createProductInput();
+        const product_id = 'product_id';
+        const user_id = 'user_id';
+
+        const product: ICreatedProduct = {
+          product_id,
+          ...productInput,
+        };
+
+        const mockedProductService: Partial<IProductService> = {
+          get: jest.fn().mockReturnValue(product),
+        };
+
+        // @ts-ignore
+        const productController = new ProductController(mockedProductService);
+
+        const req: Partial<Request & User> = {
+          params: {
+            product_id,
+          },
+          user: {
+            user_id,
+          },
+        };
+
+        // @ts-ignore
+        await productController.get(req, res);
+
+        expect(req.params).not.toBe(null);
+        expect(mockedProductService.get).toHaveBeenLastCalledWith(
+          req.user!.user_id,
+          req.params!.product_id
+        );
+        expect(res.json).toHaveBeenCalledWith(product);
+        expect(res.status).toHaveBeenCalledWith(200);
+      });
+    });
+  });
+
+  describe('getAll()', () => {
+    describe('user has a valid user_id', () => {
+      test('should return ALL the products associated to the given user_id', async () => {
+        const products = [createProductInput(), createProductInput()];
+        const user_id = 'user_id';
+
+        const mockedProductService: Partial<IProductService> = {
+          getAll: jest.fn().mockReturnValue(products),
+        };
+
+        // @ts-ignore
+        const productController = new ProductController(mockedProductService);
+
+        const req: Partial<Request & User> = {
+          user: {
+            user_id,
+          },
+        };
+
+        // @ts-ignore
+        await productController.getAll(req, res);
+
+        expect(mockedProductService.getAll).toHaveBeenLastCalledWith(
+          req.user!.user_id
+        );
+        expect(res.json).toHaveBeenCalledWith(products);
+        expect(res.status).toHaveBeenCalledWith(200);
       });
     });
   });
--- __test__/unit/product/product.service.test.ts
+++ __test__/unit/product/product.service.test.ts
@@ -2,31 +2,169 @@ import {
   ICreatedProduct,
   IProduct,
 } from '../../../src/modules/product/product.model';
-import ProductService from '../../../src/modules/product/product.service';
+import ProductService, {
+  ProductModelType,
+} from '../../../src/modules/product/product.service';
+import { createProductInput } from '../../helpers';
 
 describe('Product service test', () => {
   describe('create()', () => {
     describe("user has provided complete product and the owners' user_id", () => {
       test('should save and return the newly created product', async () => {
-        const productService = new ProductService();
+        const product: IProduct = createProductInput();
         const user_id = 'user_id';
-
-        const product: IProduct = {
-          name: 'product',
-          description: 'description',
-          price: 10.22,
-          tags: ['tag1', ' tag2'],
-        };
+        const product_id = 'product_id';
 
         const expectedProduct: ICreatedProduct = {
+          product_id,
           ...product,
-          product_id: expect.any(String),
         };
 
+        const mockProductModel: Partial<ProductModelType> = {
+          create: jest
+            .fn()
+            .mockReturnValue({ _id: product_id, ...expectedProduct }),
+        };
+
+        // @ts-ignore
+        const productService = new ProductService(mockProductModel);
         const createdProduct = await productService.create(user_id, product);
 
+        expect(mockProductModel.create).toHaveBeenCalledWith({
+          ...product,
+          user_id,
+        });
         expect(createdProduct).toStrictEqual(expectedProduct);
       });
     });
+
+    describe("user has provided complete product but EMPTY/NULL owners' user_id", () => {
+      test('should save and return the newly created product', async () => {
+        const product: IProduct = createProductInput();
+        const user_id = 'user_id';
+        const product_id = 'product_id';
+
+        const expectedProduct: ICreatedProduct = {
+          product_id,
+          ...product,
+        };
+
+        const mockProductModel: Partial<ProductModelType> = {
+          create: jest
+            .fn()
+            .mockReturnValue({ _id: product_id, ...expectedProduct }),
+        };
+
+        // @ts-ignore
+        const productService = new ProductService(mockProductModel);
+        const createdProduct = await productService.create(user_id, product);
+
+        expect(mockProductModel.create).toHaveBeenCalledWith({
+          ...product,
+          user_id,
+        });
+        expect(createdProduct).toStrictEqual(expectedProduct);
+      });
+    });
+  });
+
+  describe('get()', () => {
+    describe('user has provided product_id and user_id', () => {
+      test('should return the product based on the product_id', async () => {
+        const product: IProduct = createProductInput();
+        const user_id = 'user_id';
+        const product_id = 'product_id';
+
+        const expectedProduct: ICreatedProduct = {
+          product_id,
+          ...product,
+        };
+
+        const mockProductModel: Partial<ProductModelType> = {
+          findOne: jest
+            .fn()
+            .mockReturnValue({ _id: product_id, ...expectedProduct }),
+        };
+
+        // @ts-ignore
+        const productService = new ProductService(mockProductModel);
+        const returnedProduct = await productService.get(user_id, product_id);
+
+        expect(mockProductModel.findOne).toHaveBeenCalled();
+        expect(returnedProduct).toStrictEqual(expectedProduct);
+      });
+    });
+
+    describe('user has provided an INVALID product_id OR user_id', () => {
+      test("should throw an error 'Product not found'", async () => {
+        const user_id = '';
+        const product_id = '';
+
+        const mockProductModel: Partial<ProductModelType> = {
+          findOne: jest.fn().mockReturnValue(null),
+        };
+
+        // @ts-ignore
+        const productService = new ProductService(mockProductModel);
+
+        try {
+          await productService.get(user_id, product_id);
+        } catch (error) {
+          expect(mockProductModel.findOne).toHaveBeenCalled();
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe('Product not found');
+        }
+      });
+    });
+  });
+
+  describe('getAll()', () => {
+    describe('user has provided user_id', () => {
+      test('should return ALL the products based on the product_id', async () => {
+        const product: IProduct = createProductInput();
+        const user_id = 'user_id';
+        const product_id = 'product_id';
+
+        const expectedProducts: ICreatedProduct[] = [
+          {
+            product_id,
+            ...product,
+          },
+        ];
+
+        const mockProductModel: Partial<ProductModelType> = {
+          find: jest.fn().mockReturnValue([{ _id: product_id, ...product }]),
+        };
+
+        // @ts-ignore
+        const productService = new ProductService(mockProductModel);
+        const returnedProducts = await productService.getAll(user_id);
+
+        expect(mockProductModel.find).toHaveBeenCalledWith({ user_id });
+        expect(returnedProducts).toStrictEqual(expectedProducts);
+      });
+    });
+
+    // describe('user has provided an INVALID product_id OR user_id', () => {
+    //   test("should throw an error 'Product not found'", async () => {
+    //     const user_id = '';
+    //     const product_id = '';
+
+    //     const mockProductModel: Partial<ProductModelType> = {
+    //       findOne: jest.fn().mockReturnValue(null),
+    //     };
+
+    //     // @ts-ignore
+    //     const productService = new ProductService(mockProductModel);
+
+    //     try {
+    //       await productService.get(user_id, product_id);
+    //     } catch (error) {
+    //       expect(mockProductModel.findOne).toHaveBeenCalled();
+    //       expect(error).toBeInstanceOf(Error);
+    //       expect(error.message).toBe('Product not found');
+    //     }
+    //   });
+    // });
   });
 });
