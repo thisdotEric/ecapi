c36854f19f133caccf43e8f8b5f37f4f13aaa1eb
thisdotEric <siguenzajohneric@gmail.com>
2022-08-07 17:27:05 +0800

    Added jsonwebtoken for authentication

---
+++ src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import { sign } from 'jsonwebtoken';
+
+// The payload (for now) is just the user_id
+
+export const generateAccessToken = (user_id: string): string => {
+  const accessToken = sign(user_id, `${process.env.ACCESS_TOKEN_SECRET}`, {
+    expiresIn: '7d',
+  });
+  return accessToken;
+};
+
+export const generateRefreshToken = (user_id: string): string => {
+  const refreshToken = sign(user_id, `${process.env.ACCESS_TOKEN_SECRET}`);
+  return refreshToken;
+};
+
+// export const verify = async (payload: string) => {};
