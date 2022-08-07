d9bd2d373e1f03af3062288589772e4f26f44bd0
thisdotEric <siguenzajohneric@gmail.com>
2022-08-07 20:36:32 +0800

    Added unit tests for user service class login method

--- __test__/unit/user/user.service.test.ts
+++ __test__/unit/user/user.service.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { ICreatedUser, User } from '../../../src/modules/user/user.model';
 import UserService from '../../../src/modules/user/user.service';
 import { createUserInput } from '../../helpers';
+import { ITokens } from '../../../src/modules/user/user.interface';
 
 describe('User Service tests', () => {
   describe('create()', () => {
@@ -126,6 +127,111 @@ describe('User Service tests', () => {
           expect(mockedUserModel.findByIdAndDelete).toBeCalledTimes(1);
           expect(error).toBeInstanceOf(Error);
           expect(error.message).toBe('Unable to delete user');
+        }
+      });
+    });
+  });
+
+  describe('login()', () => {
+    describe('user has provided both email and password', () => {
+      test('should return an object with access token and refresh token', async () => {
+        const email = 'johndoe@gmail.com';
+        const password = 'password';
+        const salt = 'samplesalt';
+        const hashedPassword = 'samplehashedpassword';
+
+        const mockTokens: ITokens = {
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String),
+        };
+
+        const mockedUserModel: Partial<
+          ReturnModelType<typeof User, BeAnObject>
+        > = {
+          findOne: jest.fn().mockReturnValue({
+            _id: new mongoose.Types.ObjectId(),
+            name: 'name',
+            email,
+            salt,
+            hashedPassword,
+          }),
+        };
+
+        // @ts-ignore
+        const userService = new UserService(mockedUserModel);
+        let actualTokens;
+
+        const mockVerifyPasswordFn = jest.fn().mockReturnValue(true);
+
+        // @ts-ignore
+        actualTokens = await userService.login(
+          email,
+          password,
+          mockVerifyPasswordFn
+        );
+
+        expect(mockedUserModel.findOne).toHaveBeenCalled();
+        expect(mockVerifyPasswordFn).toHaveBeenCalledWith(
+          password,
+          hashedPassword,
+          salt
+        );
+        expect(actualTokens).toStrictEqual(mockTokens);
+      });
+    });
+
+    describe('user has provided both email and password but the password is incorrect', () => {
+      test("should throw an error 'Login failed'", async () => {
+        const email = 'johndoe@gmail.com';
+        const password = 'incorrectPassword';
+
+        const mockedUserModel: Partial<
+          ReturnModelType<typeof User, BeAnObject>
+        > = {
+          findOne: jest.fn().mockReturnValue({
+            _id: new mongoose.Types.ObjectId(),
+            name: 'name',
+            email,
+            salt: 'invalidsalt',
+            hashedPassword: 'wronghashedpassword',
+          }),
+        };
+
+        // @ts-ignore
+        const userService = new UserService(mockedUserModel);
+
+        const mockVerifyPasswordFn = jest.fn().mockReturnValue(false);
+
+        try {
+          await userService.login(email, password, mockVerifyPasswordFn);
+        } catch (error) {
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe('Login failed');
+          expect(mockedUserModel.findOne).toHaveBeenCalled();
+        }
+      });
+    });
+
+    describe('user has provided both email and password but the user does not exists', () => {
+      test("should throw an error 'User does not exists'", async () => {
+        const email = 'userdoesnotexists@gmail.com';
+        const password = '';
+
+        const mockedUserModel: Partial<
+          ReturnModelType<typeof User, BeAnObject>
+        > = {
+          findOne: jest.fn().mockReturnValue(null),
+        };
+
+        // @ts-ignore
+        const userService = new UserService(mockedUserModel);
+
+        try {
+          await userService.login(email, password);
+        } catch (error) {
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe('User does not exists');
+          expect(mockedUserModel.findOne).toHaveBeenCalled();
         }
       });
     });
