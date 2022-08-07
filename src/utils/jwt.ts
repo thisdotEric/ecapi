f2725ac7b822f873c4b189235acea6abf0b1f02e
thisdotEric <siguenzajohneric@gmail.com>
2022-08-07 20:35:01 +0800

    Updated setting payload on jwt sign method

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,14 +3,14 @@ import { sign } from 'jsonwebtoken';
 // The payload (for now) is just the user_id
 
 export const generateAccessToken = (user_id: string): string => {
-  const accessToken = sign(user_id, `${process.env.ACCESS_TOKEN_SECRET}`, {
+  const accessToken = sign({ user_id }, `${process.env.ACCESS_TOKEN_SECRET}`, {
     expiresIn: '7d',
   });
   return accessToken;
 };
 
 export const generateRefreshToken = (user_id: string): string => {
-  const refreshToken = sign(user_id, `${process.env.ACCESS_TOKEN_SECRET}`);
+  const refreshToken = sign({ user_id }, `${process.env.ACCESS_TOKEN_SECRET}`);
   return refreshToken;
 };
 
