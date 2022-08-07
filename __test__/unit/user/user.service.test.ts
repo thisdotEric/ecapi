c69b9ca0dcae9102169f1f41334a19b6dfe84127
thisdotEric <siguenzajohneric@gmail.com>
2022-08-07 15:06:23 +0800

    Added unit test for get method in user service class

--- __test__/unit/user/user.service.test.ts
+++ __test__/unit/user/user.service.test.ts
@@ -6,28 +6,82 @@ import UserService from '../../../src/modules/user/user.service';
 import { createUserInput } from '../../helpers';
 
 describe('User Service tests', () => {
-  test('should create a new user', async () => {
-    const newUserInput = createUserInput();
+  describe('create()', () => {
+    test('should create a new user', async () => {
+      const newUserInput = createUserInput();
 
-    const expectedUser: ICreatedUser<string> = {
-      id: expect.any(String),
-      name: newUserInput.name,
-      email: newUserInput.email,
-    };
-
-    const mockedUserModel: Partial<ReturnModelType<typeof User, BeAnObject>> = {
-      create: jest.fn().mockReturnValue({
-        _id: new mongoose.Types.ObjectId(),
+      const expectedUser: ICreatedUser<string> = {
+        id: expect.any(String),
         name: newUserInput.name,
         email: newUserInput.email,
-      }),
-    };
+      };
 
-    // @ts-ignore
-    const userService = new UserService(mockedUserModel);
-    const actualCreatedUser = await userService.create(newUserInput);
+      const mockedUserModel: Partial<ReturnModelType<typeof User, BeAnObject>> =
+        {
+          create: jest.fn().mockReturnValue({
+            _id: new mongoose.Types.ObjectId(),
+            name: newUserInput.name,
+            email: newUserInput.email,
+          }),
+        };
 
-    expect(mockedUserModel.create).toBeCalledTimes(1);
-    expect(actualCreatedUser).toEqual(expectedUser);
+      // @ts-ignore
+      const userService = new UserService(mockedUserModel);
+      const actualCreatedUser = await userService.create(newUserInput);
+
+      expect(mockedUserModel.create).toBeCalledTimes(1);
+      expect(actualCreatedUser).toEqual(expectedUser);
+    });
+  });
+
+  describe('get()', () => {
+    describe('user has provided a valid user_id', () => {
+      test('return the user associated with the user_id', async () => {
+        const user = createUserInput();
+
+        const expectedUser: ICreatedUser<string> = {
+          id: expect.any(String),
+          name: user.name,
+          email: user.email,
+        };
+
+        const mockedUserModel: Partial<
+          ReturnModelType<typeof User, BeAnObject>
+        > = {
+          findById: jest.fn().mockReturnValue({
+            _id: new mongoose.Types.ObjectId(),
+            name: user.name,
+            email: user.email,
+          }),
+        };
+
+        // @ts-ignore
+        const userService = new UserService(mockedUserModel);
+        const actualUser = await userService.get('user_id');
+
+        expect(mockedUserModel.findById).toBeCalledTimes(1);
+        expect(actualUser).toEqual(expectedUser);
+      });
+    });
+
+    describe('user has provided an invalid user_id', () => {
+      test("should throw an error 'User not found'", async () => {
+        const mockedUserModel: Partial<
+          ReturnModelType<typeof User, BeAnObject>
+        > = {
+          findById: jest.fn().mockReturnValue(null),
+        };
+        // @ts-ignore
+        const userService = new UserService(mockedUserModel);
+
+        try {
+          await userService.get('invalid_id');
+        } catch (error) {
+          expect(mockedUserModel.findById).toBeCalledTimes(1);
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe('User not found');
+        }
+      });
+    });
   });
 });
